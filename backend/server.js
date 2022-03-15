903906adf958a0113f74d69d7486fa510e96f393
cshepscorp <sheppard.christy@gmail.com>
2022-03-14 20:14:14 -0400

    deploy fix in server.js

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,11 @@ app.use('/api/tickets', require('./routes/ticketRoutes'));
 if (process.env.NODE_ENV === 'production') {
   // set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')));
+
   //
-  app.get('*', (erq, res) =>
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  );
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
 } else {
   app.get('/', (req, res) => {
     res.json({ message: 'Welcome to support desk API' });
